9c297f68bb355ef1d29e5afdd1695cea27dec5b9
Yusuf Can <34337184+VivereYC@users.noreply.github.com>
2022-12-08 16:16:01 +0100

    Update configure_ntp.sh

--- configure_ntp.sh
+++ configure_ntp.sh
@@ -9,6 +9,6 @@
 
 sed -i -r -e "s/^((server|pool).*)/# \1         # commented by $(whoami) on $(date +"%Y-%m-%d @ %H:%M:%S")/" /etc/ntp.conf
 
-echo -e "\npool pool.ntp.org iburst         # added by $(whoami) on $(date +"%Y-%m-%d @ %H:%M:%S")" | tee -a /etc/ntp.conf
+echo -e "\npool pool.ntp.org iburst             # added by $(whoami) on $(date +"%Y-%m-%d @ %H:%M:%S")" | tee -a /etc/ntp.conf
 
 service ntp restart
